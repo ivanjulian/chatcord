9953e9579ffd6a891ac1e0189623eb081a2df875
Ivan Khizhnyak <vanja.khizhnjak99@gmail.com>
2020-11-09 09:58:26 +0200

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,34 @@
 const chatForm = document.getElementById('chat-form');
 const chatMessages = document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
+
+// Get username and room from url
+const qs = window.location.search.split('&');
+const qsSearch = (toSearch) => {
+  let result = 0;
+  qs.map((item) => {
+    if (item.includes(toSearch)) {
+      result = item.split('=')[1];
+    }
+  });
+
+  return result;
+};
+
+const username = qsSearch('username');
+const room = qsSearch('room');
 
 const socket = io();
+
+// Join chat
+socket.emit('joinRoom', { username, room });
+
+//Get room and users
+socket.on('roomUsers', ({ room, users }) => {
+  outputRoomName(room);
+  outputUsers(users);
+});
 
 socket.on('message', (message) => {
   console.log('message from Server: ', message);
@@ -28,10 +55,22 @@ chatForm.addEventListener('submit', (e) => {
 // Output message to DOM
 const outputMessage = (message) => {
   const div = document.createElement('div');
-  div.innerHTML = `<p class="meta">Brad<span>9:12pm</span></p> 
+  div.innerHTML = `<p class="meta">${message.username}<span>${message.time}</span></p> 
     <p class="text" >
-    ${message}
+    ${message.text}
     </p>
   `;
   document.querySelector('.chat-messages').appendChild(div);
+};
+
+// Add roomname to DOM
+const outputRoomName = (room) => {
+  roomName.innerText = room;
+};
+
+// Add users to DOM
+const outputUsers = (users) => {
+  userList.innerText = ` 
+    ${users.map((user) => `<li>${user.username}</li>`).join('')}
+  `;
 };
