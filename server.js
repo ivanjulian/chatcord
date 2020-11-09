0648b91d0b75bd69a90a3515ff11ecc6ae9e30cf
Ivan Khizhnyak <vanja.khizhnjak99@gmail.com>
2020-11-09 09:58:20 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
+const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/users');
+const users = require('./utils/users');
 
 const app = express();
 const server = http.createServer(app);
@@ -9,22 +12,39 @@ const io = socketio(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+const admin = 'Admin';
+
 io.on('connection', (socket) => {
   console.log('New WS connection');
 
-  socket.emit('message', 'Welcome to ChatWS !');
+  socket.on('joinRoom', ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+    socket.join(room);
+    // Welcoming the user
+    socket.emit('message', formatMessage(admin, 'Welcome to ChatWS !'));
 
-  // Broadcast when a user connects
-  socket.broadcast.emit('message', 'A user has joined the chat');
+    // Broadcast when a user connects
+    socket.broadcast.to(room).emit('message', formatMessage(admin, `${username} has joined the chat`));
 
-  // Runs when client disconnects
-  socket.on('disconnect', () => {
-    io.emit('message', 'A user has left the chat');
+    // Send users and room info
+    io.to(user.room).emit('roomUsers', {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
   });
 
   //Listen for chatMessage
   socket.on('chatMessage', (msg) => {
-    io.emit('message', msg);
+    const { username, room } = getCurrentUser(socket.id);
+    io.to(room).emit('message', formatMessage(username, msg));
+  });
+
+  // Runs when client disconnects
+  socket.on('disconnect', () => {
+    const user = userLeave(socket.id);
+    if (user) {
+      io.to(user.room).emit('message', formatMessage(admin, `${user.username} has left the chat`));
+    }
   });
 });
 
